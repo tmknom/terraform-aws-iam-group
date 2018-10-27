718cd2915ae47cb02a1aa8cd669c9a4c40315e99
tmknom <tmk.nom@gmail.com>
2018-10-28 01:33:13 +0900

    Add users to minimal example

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,8 +1,15 @@
 module "iam_group" {
   source = "../../"
   name   = "minimal"
-
   policy = "${data.aws_iam_policy_document.policy.json}"
+
+  users = [
+    "${aws_iam_user.user.name}",
+  ]
+}
+
+resource "aws_iam_user" "user" {
+  name = "minimal_user"
 }
 
 data "aws_iam_policy_document" "policy" {
